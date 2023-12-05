cc47a14386379787cbe8d61f42ad47a19f29e07a
TomasFernandez1 <tomasefernandez1@gmail.com>
2023-12-05 11:19:13 -0300

    Desafio2

---
+++ main.mjs
@@ -0,0 +1,16 @@
+import { ProductManager } from "./ProductManager.mjs";
+
+const pManager = new ProductManager();
+
+pManager.getProducts()
+
+/*
+pManager.addProduct({
+    title: 'product_1',
+    description: 'testing',
+    price: 20,
+    thumbnail: 'test',
+    code: 'testingCode2',
+    stock: 1
+})
+*/
